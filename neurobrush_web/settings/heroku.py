ed4d372792e9159ff70a546a3ce2a26580dd4416
Ilan Bar-Magen <ilanbarmagen@gmail.com>
2013-10-11 19:16:55 +0300

    heroku

--- neurobrush_web/settings/heroku.py
+++ neurobrush_web/settings/heroku.py
@@ -4,14 +4,27 @@ from .base import *
 import dj_database_url
 
 ENV = 'HEROKU'
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = [
     'neurobrush.com'
 ]
 # ALLOWED_HOSTS = ['*']
 
-DATABASES = {	
-    'default': dj_database_url.config()
+# DATABASES = {	
+#     'default': dj_database_url.config()
+# }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
 }
 
 INSTALLED_APPS += (
