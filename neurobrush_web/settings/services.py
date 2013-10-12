2e0d607dc44bf70d1040916d5c5dcb145b7c259a
Ilan Bar-Magen <ilanbarmagen@gmail.com>
2013-10-12 10:03:18 +0300

    s3

--- neurobrush_web/settings/services.py
+++ neurobrush_web/settings/services.py
@@ -4,9 +4,9 @@ from os import environ
 #S3
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 #TODO: env vars
-AWS_ACCESS_KEY_ID ='AKIAJB7WPKNNL2AMBH5A' # os.environ.get('AWS_ACCESS_KEY_ID') #
-AWS_SECRET_ACCESS_KEY =  'Y8wglUGUJNeEfkZNYZDNMpFKlzAmmsA9LnBq1Dx' #os.environ.get('AWS_SECRET_ACCESS_KEY') #
-AWS_STORAGE_BUCKET_NAME = 'neurobrush'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID') #
+AWS_SECRET_ACCESS_KEY =  os.environ.get('AWS_SECRET_ACCESS_KEY') #
+AWS_STORAGE_BUCKET_NAME = 'neurobrush-static'
 #AWS_CALLING_FORMAT = ''
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_QUERYSTRING_AUTH = False
