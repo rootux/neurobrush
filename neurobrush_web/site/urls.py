2f6f2b18c2422c6babb9e4acfc4f3c5337054170
Gal Bracha <galbra@gmail.com>
2013-10-12 14:46:17 +0300

    typo

--- neurobrush_web/site/urls.py
+++ neurobrush_web/site/urls.py
@@ -6,7 +6,7 @@ from neurobrush_web.site import views
 urlpatterns = patterns('',
     url(r'^$', views.start, name='start'),
     url(r'^collect$', views.collect, name='collect'),
-    url(r'^sockets$', views.sockets, name='sockets')
+    url(r'^sockets$', views.sockets, name='sockets'),
     url(r'^getlatest$', views.getLatestData, name='getlatest')
     # url(r'^$', views.category, name='category')
 )
