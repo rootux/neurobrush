eed7c355d8d5aa4e08b7e1948df5c0f51e2c1bbc
Gal Bracha <galbra@gmail.com>
2013-10-12 14:38:08 +0300

    added redis to store the data

--- neurobrush_web/site/collect.py
+++ neurobrush_web/site/collect.py
@@ -1,13 +1,17 @@
-import json
 import ast
+import redis
+
+REDIS_URL = 'redis://redistogo:ed568b706b4474886cf3bbd5161e2166@tarpon.redistogo.com:10054'
 
 def collectData(request):
-    array = request.POST.items()[0][0]
-    #array is a string
-    #array = [s.replace('{', '') for s in array]
-    #array = [s.replace('}', '') for s in array]
-    splitted = eval(array)
+    
+    #TODO!!!
+    redis = redis.from_url(REDIS_URL)
 
+    array = request.POST.items()[0][0]
+    splitted = eval(array)
+    redis.set('ExcitementShortTerm', splitted['ExcitementShortTerm'])
+    #my_global = splitted['ExcitementShortTerm']
     print splitted['ExcitementShortTerm']
     #json.loads(splitted)
     #excitementShortTerm = request.POST['ExcitementShortTerm']
