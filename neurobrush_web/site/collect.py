d853fd633f5b9a79efcc2d7490ed1cdce115ea97
Gal Bracha <galbra@gmail.com>
2013-10-12 14:54:59 +0300

    updated redis global

--- neurobrush_web/site/collect.py
+++ neurobrush_web/site/collect.py
@@ -3,10 +3,11 @@ import redis
 
 REDIS_URL = 'redis://redistogo:ed568b706b4474886cf3bbd5161e2166@tarpon.redistogo.com:10054'
 
+redis = redis.from_url(REDIS_URL)
 def collectData(request):
     
     #TODO!!!
-    redis = redis.from_url(REDIS_URL)
+    global redis
 
     array = request.POST.items()[0][0]
     splitted = eval(array)
--- neurobrush_web/site/getlatestdata.py
+++ neurobrush_web/site/getlatestdata.py
@@ -2,9 +2,10 @@ import redis
 
 REDIS_URL = 'redis://redistogo:ed568b706b4474886cf3bbd5161e2166@tarpon.redistogo.com:10054'
 
+redis = redis.from_url(REDIS_URL)
 def getlatest(request):
     
     #TODO!!!
-    redis = redis.from_url(REDIS_URL)
+    global redis
 
     return redis.get('ExcitementShortTerm')
