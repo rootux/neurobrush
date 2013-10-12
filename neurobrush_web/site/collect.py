e79b191c583534b032108f8fe16601b0e7d2b15a
Gal Bracha <galbra@gmail.com>
2013-10-12 16:41:48 +0300

    updated redis

--- neurobrush_web/site/collect.py
+++ neurobrush_web/site/collect.py
@@ -1,6 +1,7 @@
 import ast
 import redis
 
+#REDIS_URL = 'redis://localhost:6379'
 REDIS_URL = 'redis://redistogo:ed568b706b4474886cf3bbd5161e2166@tarpon.redistogo.com:10054'
 
 redis = redis.from_url(REDIS_URL)
@@ -16,7 +17,7 @@ def collectData(request):
     splitted = eval(array)
     print splitted
     redis.set('ExcitementShortTerm', splitted['ExcitementShortTerm'])
-    redis.set('ExcitementLongTerm', splitted['ExcitementLongTerm'])
+    #redis.set('ExcitementLongTerm', splitted['ExcitementLongTerm'])
     redis.set('EngagementBoredom', splitted['EngagementBoredom'])
     redis.set('FrustrationScore', splitted['FrustrationScore'])
     redis.set('Lowerface', splitted['Lowerface'])
--- neurobrush_web/site/getlatestdata.py
+++ neurobrush_web/site/getlatestdata.py
@@ -1,5 +1,6 @@
 import redis
 
+#REDIS_URL = 'redis://localhost:6379'
 REDIS_URL = 'redis://redistogo:ed568b706b4474886cf3bbd5161e2166@tarpon.redistogo.com:10054'
 
 redis = redis.from_url(REDIS_URL)
